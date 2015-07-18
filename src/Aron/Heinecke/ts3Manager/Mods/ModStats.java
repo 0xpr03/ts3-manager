8d76e97c25d66d5004bc1acaf5ceda6ddbd68b6b
Aron Heinecke <aron.heinecke@t-online.de>
2015-07-18 21:58:36 +0200

    modstats basic layout *logger config fix

---
+++ src/Aron/Heinecke/ts3Manager/Mods/ModStats.java
@@ -0,0 +1,72 @@
+package Aron.Heinecke.ts3Manager.Mods;
+
+import java.util.HashMap;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import Aron.Heinecke.ts3Manager.Lib.API.ModEvent;
+import Aron.Heinecke.ts3Manager.Lib.API.TS3Event;
+
+public class ModStats implements ModEvent, TS3Event {
+	Logger logger;
+	
+	public ModStats(){
+		logger = LogManager.getLogger();
+	}
+
+	@Override
+	public void handleClientJoined(String eventType, HashMap<String, String> eventInfo) {
+		logger.debug("Client joined, {}",eventInfo);
+	}
+
+	@Override
+	public void handleClientLeft(String eventType, HashMap<String, String> eventInfo) {
+		logger.debug("Client left, {}",eventInfo);
+	}
+
+	@Override
+	public boolean needs_Event_Channel() {
+		return false;
+	}
+
+	@Override
+	public boolean needs_Event_Server() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean needs_Event_TextChannel() {
+		return false;
+	}
+
+	@Override
+	public boolean needs_Event_TextPrivate() {
+		return false;
+	}
+
+	@Override
+	public boolean needs_Event_TextServer() {
+		return false;
+	}
+
+	@Override
+	public boolean needs_MYSQL() {
+		return true;
+	}
+
+	@Override
+	public void handleReady() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleClientMoved(String eventType, HashMap<String, String> eventInfo) {
+	}
+	
+	@Override
+	public void handleTextMessage(String eventType, HashMap<String, String> eventInfo) {
+	}
+}
