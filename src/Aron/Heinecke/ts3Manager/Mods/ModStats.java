c8c99dd8242fc9f454bf0034e39fdf628f90a2fe
Aron Heinecke <aron.heinecke@t-online.de>
2015-07-23 18:20:32 +0200

    *moved cache insert to nonblocking by ignoring data flood +data pool for ModStats for interval based insert -instance managed mysql conn

--- src/Aron/Heinecke/ts3Manager/Mods/ModStats.java
+++ src/Aron/Heinecke/ts3Manager/Mods/ModStats.java
@@ -2,14 +2,17 @@ package Aron.Heinecke.ts3Manager.Mods;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.Vector;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import Aron.Heinecke.ts3Manager.Instance;
+import Aron.Heinecke.ts3Manager.Lib.MYSQLConnector;
 import Aron.Heinecke.ts3Manager.Lib.API.ModEvent;
 import Aron.Heinecke.ts3Manager.Lib.API.TS3Event;
 import de.stefan1200.jts3serverquery.JTS3ServerQuery;
@@ -25,23 +28,58 @@ public class ModStats implements ModEvent, TS3Event {
 	private Instance<?> instance;
 	private Timer timer;
 	private TimerTask timerdosnapshot;
+	private Timer bufferTimer;
+	private TimerTask taskBuffer;
 	private PreparedStatement stm;
 	private String sql;
 	private String tableName;
-	
-	public ModStats(Instance<?> instance){
+	private MYSQLConnector conn;
+	private Vector<DatElem> buffer = new Vector<DatElem>();
+
+	public ModStats(Instance<?> instance) {
 		this.instance = instance;
-		logger.debug("Instance: {}",this.instance.getSID());
-		tableName = "ModStats_"+instance.getSID();
-		sql = String.format("INSERT INTO %s (`clients`,`queryclients`) VALUES (?,?);",tableName);
+		logger.debug("Instance: {}", this.instance.getSID());
+		tableName = "ModStats_" + instance.getSID();
+		sql = String.format("INSERT INTO %s (`timestamp`,`clients`,`queryclients`) VALUES (?,?,?);", tableName);
 		timerdosnapshot = new TimerTask() {
 			@Override
 			public void run() {
-				scheduleUpdate();
+				addUpdate();
 			}
 		};
+		taskBuffer = new TimerTask() {
+			@Override
+			public void run() {
+				insertBuffer();
+			}
+		};
+		bufferTimer = new Timer(true);
+		bufferTimer.schedule(taskBuffer, 15*60*1000,15*60*1000); // 15 min
 	}
 	
+	/**
+	 * Flushes the current buffer into the DB
+	 */
+	private void insertBuffer(){
+		try{
+			long time = System.currentTimeMillis();
+			conn = new MYSQLConnector();
+			stm = conn.prepareStm(sql);
+			Vector<DatElem> copyBuffer = buffer; // non-deep copy
+			for(DatElem de : copyBuffer){
+				stm.setTimestamp(1, de.getTimestamp());
+				stm.setInt(2, de.getClients());
+				stm.setInt(3, de.getQueryclients());
+				buffer.remove(de);
+			}
+			stm.close();
+			conn.disconnect();
+			logger.debug("Buffer flushed in {} MS, {} entrys",System.currentTimeMillis() - time,copyBuffer.size());
+		}catch(SQLException e){
+			logger.error(e);
+		}
+	}
+
 	/**
 	 * Request update<br>
 	 * Lazy scheduling stops rapid updates on massive join/leaves
@@ -49,8 +87,8 @@ public class ModStats implements ModEvent, TS3Event {
 	private void updateClients() {
 		if(System.currentTimeMillis() - last_update >= 1000){
 			last_update = System.currentTimeMillis();
-			scheduleUpdate();
-		}else{
+			addUpdate();
+		} else {
 			logger.debug("Scheduling later");
 			timer = new Timer(false);
 			timer.schedule(timerdosnapshot, 1000);
@@ -60,15 +98,12 @@ public class ModStats implements ModEvent, TS3Event {
 	/**
 	 * Internal update scheduler, shouldn't be called directly
 	 */
-	private void scheduleUpdate(){
+	private void addUpdate() {
 		try {
-			long start = System.currentTimeMillis();
-			HashMap<String,String> i = getInfo();
-			stm.setInt(1, Integer.valueOf(i.get("virtualserver_clientsonline")));
-			stm.setInt(2, Integer.valueOf(i.get("virtualserver_queryclientsonline")));
-			stm.executeUpdate();
-			logger.debug("insert took {} ms",System.currentTimeMillis() - start);
-		} catch (TS3ServerQueryException | SQLException e) {
+			HashMap<String, String> i = getInfo();
+			buffer.add(new DatElem(Integer.valueOf(i.get("virtualserver_clientsonline")),
+					Integer.valueOf(i.get("virtualserver_queryclientsonline"))));
+		} catch (TS3ServerQueryException e) {
 			logger.error(e);
 		}
 	}
@@ -121,27 +156,16 @@ public class ModStats implements ModEvent, TS3Event {
 	}
 
 	@Override
-	public boolean needs_MYSQL() {
-		return true;
-	}
-
-	@Override
 	public void handleReady() {
-		try{
-			String table = String.format("CREATE TABLE IF NOT EXISTS `%s` ("
-				+" `clients` int(11) NOT NULL,"
-				+" `queryclients` int(11) NOT NULL,"
-				+" `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
-				+" PRIMARY KEY (`timestamp`)"
-				+") ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='%s'",tableName,instance.getTS3Connection().getConnector().getInfo(JTS3ServerQuery.INFOMODE_SERVERINFO, 0).get("virtualserver_name"));
-			instance.getMysqlconnector().execUpdateQuery(table);
-		}catch(SQLException | TS3ServerQueryException e){
-			logger.error("{}",e);
-		}
 		try {
-			stm = instance.getMysqlconnector().prepareStm(sql);
-		} catch (SQLException e) {
-			logger.error("{}",e);
+			String table = String.format("CREATE TABLE IF NOT EXISTS `%s` (" + " `clients` int(11) NOT NULL,"
+					+ " `queryclients` int(11) NOT NULL," + " `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+					+ " PRIMARY KEY (`timestamp`)"
+					+ ") ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='%s'", tableName, instance.getTS3Connection()
+							.getConnector().getInfo(JTS3ServerQuery.INFOMODE_SERVERINFO, 0).get("virtualserver_name"));
+			new MYSQLConnector().execUpdateQuery(table);
+		} catch (SQLException | TS3ServerQueryException e) {
+			logger.error("{}", e);
 		}
 		updateClients();
 	}
@@ -149,7 +173,7 @@ public class ModStats implements ModEvent, TS3Event {
 	@Override
 	public void handleClientMoved(HashMap<String, String> eventInfo) {
 	}
-	
+
 	@Override
 	public void handleTextMessage(String eventType, HashMap<String, String> eventInfo) {
 	}
@@ -158,11 +182,44 @@ public class ModStats implements ModEvent, TS3Event {
 	public void handleShutdown() {
 		logger.entry();
 		timerdosnapshot.cancel();
-		if(timer != null)
+		if (timer != null)
 			timer.cancel();
 		try {
-			if(stm != null)
-				stm.close();
-		} catch (SQLException e) {}
+			if (stm != null){
+				if (!stm.isClosed())
+					stm.close();
+			}
+			conn.disconnect();
+		} catch (SQLException e) {
+		}
+		taskBuffer.cancel();
+		if(bufferTimer != null) 
+			bufferTimer.cancel();
+		insertBuffer();
+		logger.exit();
+	}
+
+	class DatElem {
+		private Timestamp timestamp;
+		private int clients;
+		private int queryclients;
+
+		public DatElem(int clients, int queryclients) {
+			this.timestamp = new Timestamp(System.currentTimeMillis());
+			this.clients = clients;
+			this.queryclients = queryclients;
+		}
+
+		public Timestamp getTimestamp() {
+			return timestamp;
+		}
+
+		public int getClients() {
+			return clients;
+		}
+
+		public int getQueryclients() {
+			return queryclients;
+		}
 	}
 }
