b0849fc211cc93acc6833f28f63a97613dc59609
Aron Heinecke <aron.heinecke@t-online.de>
2015-07-30 00:18:20 +0200

    *fixed params +current msg sender id var +missing catches & loggings

--- src/Aron/Heinecke/ts3Manager/Mods/ModTools.java
+++ src/Aron/Heinecke/ts3Manager/Mods/ModTools.java
@@ -5,14 +5,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import Aron.Heinecke.ts3Manager.Instance;
 import Aron.Heinecke.ts3Manager.Lib.TS3Connector;
 import Aron.Heinecke.ts3Manager.Lib.API.ModEvent;
 import Aron.Heinecke.ts3Manager.Lib.API.TS3Event;
 import de.stefan1200.jts3serverquery.JTS3ServerQuery;
+import de.stefan1200.jts3serverquery.TS3ServerQueryException;
 
 public class ModTools implements ModEvent, TS3Event {
-	private Instance instance;
+	private Instance<?> instance;
+	private Logger logger = LogManager.getLogger();
+	
 	public ModTools(Instance<?> instance){
 		this.instance = instance;
 	}
@@ -23,10 +29,11 @@ public class ModTools implements ModEvent, TS3Event {
 	 * @param ap
 	 * @return
 	 */
-	private boolean rocketchan(String target, int ap) {
+	private void rocketchan(String target, int ap) {
+		TS3Connector<?> ts3conn = instance.getTS3Connection();
 		try {
-			TS3Connector<?> ts3conn = instance.getTS3Connection();
-			int tid = Integer.parseInt(args[1]); // get targetid
+			
+			int tid = Integer.parseInt(target); // get targetid
 			HashMap<String, String> tmap = ts3conn.getConnector().getInfo(JTS3ServerQuery.INFOMODE_CLIENTINFO, tid); // target infos
 			if ( tmap.get("client_database_id") == null ) {
 				throw new NumberFormatException();
@@ -34,7 +41,7 @@ public class ModTools implements ModEvent, TS3Event {
 				throw new NumberFormatException();
 			}
 			List<Integer> channels = new ArrayList<Integer>();
-			Vector<HashMap<String, String>> a = query.getList(2);
+			Vector<HashMap<String, String>> a = ts3conn.getConnector().getList(JTS3ServerQuery.LISTMODE_CHANNELLIST);
 			for ( HashMap<String, String> b : a ) {
 				channels.add(Integer.valueOf(b.get("cid")));
 			}
@@ -51,22 +58,27 @@ public class ModTools implements ModEvent, TS3Event {
 			}
 			ts3conn.getConnector().kickClient(tid, false, "You where rocketed!");
 			ts3conn.getConnector().sendTextMessage(ap, JTS3ServerQuery.TEXTMESSAGE_TARGET_CLIENT, "[b]Client " + tmap.get("client_nickname")+ "[" + tmap.get("client_database_id") + "] wurde rocketed![/b]");
-			return true;
 		} catch ( NumberFormatException e ) {
-			query.sendTextMessage(ap, JTS3ServerQuery.TEXTMESSAGE_TARGET_CLIENT, "[b]Client " + args[1] + " nicht gefunden![/b]");
-			return false;
+			try {
+				ts3conn.getConnector().sendTextMessage(ap, JTS3ServerQuery.TEXTMESSAGE_TARGET_CLIENT, "[b]Client " + target + " nicht gefunden![/b]");
+			} catch (TS3ServerQueryException e1) {
+				logger.fatal(e1);
+			}
+		} catch (TS3ServerQueryException e) {
+			logger.error("Error on rocket \n{}",e);
 		}
 	}
 
 	@Override
 	public void handleTextMessage(String eventType, HashMap<String, String> eventInfo) {
-		String[] args = eventInfo.get("msg");
+		String[] args = eventInfo.get("msg").split(" ");
+		int sID = Integer.parseInt(eventInfo.get("invokerid"));
 		if(args[0].equals("!tools")){
 			boolean misCMD = false;
 			switch(args[1]){
 			case "rocket":
 				if(args.length == 3)
-					
+					rocketchan(args[2], sID);
 				else
 					misCMD = true;
 				break;
