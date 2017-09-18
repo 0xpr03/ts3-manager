523fb1db1922a07821159ecc586058ba57dc2821
Aron Heinecke <aron.heinecke@t-online.de>
2017-09-18 16:15:32 +0200

    ModTools: Hardened rocket bot against early rejoin

--- src/Aron/Heinecke/ts3Manager/Mods/ModTools.java
+++ src/Aron/Heinecke/ts3Manager/Mods/ModTools.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -29,10 +30,15 @@ import de.stefan1200.jts3serverquery.JTS3ServerQuery;
 import de.stefan1200.jts3serverquery.TS3ServerQueryException;
 
 public class ModTools implements Mod {
+	private static final String CLIENT_DB_KEY = "client_database_id";
+	private static final String CLIENT_ID_KEY = "clid";
+	// private static final String CLIENT_UID_KEY = "client_unique_identifier";
 	private Instance instance;
 	private Logger logger = LogManager.getLogger();
 	private String CMD_HELP = "TS3Manager - ToolsMod\n!tools rocket [client_id] [ignore]\nuse [ignore] to rocket through used channels.";
 	private AtomicBoolean rocketRunning = new AtomicBoolean(false);
+	private AtomicInteger clientID = new AtomicInteger(-1);
+	private AtomicInteger dbID = new AtomicInteger(-1);
 
 	private final String PERM_MSG = "Not enough permissions to perform this action !";
 
@@ -52,8 +58,9 @@ public class ModTools implements Mod {
 	 * @param ignore_clients
 	 *            set to true to move also through channels with clients
 	 */
-	private void rocketchan(String target, int applicant, boolean ignore_clients) {
-		if (!rocketRunning.compareAndSet(false, true)) {
+	private void rocketchan(AtomicInteger target, AtomicInteger dbID, AtomicBoolean running, int applicant,
+			boolean ignore_clients) {
+		if (!running.compareAndSet(false, true)) {
 			try {
 				this.instance.getTS3Connection().getConnector().sendTextMessage(applicant,
 						JTS3ServerQuery.TEXTMESSAGE_TARGET_CLIENT, "[b]Already in use![/b]");
@@ -64,18 +71,16 @@ public class ModTools implements Mod {
 		}
 		TS3Connector<?> ts3conn = instance.getNewTS3Connector(null, "Rocket", -1);
 		try {
-			int tid = Integer.parseInt(target); // get targetid
 			if (vetoActive) {
-				tid = applicant;
-				vetoActive = false;
+				target.set(applicant);
 			}
-			HashMap<String, String> tmap = ts3conn.getConnector().getInfo(JTS3ServerQuery.INFOMODE_CLIENTINFO, tid); // target
-																														// infos
-			if (tmap.get("client_database_id") == null) {
-				throw new NumberFormatException();
-			} else if (tmap.get("client_database_id") == "") {
-				throw new NumberFormatException();
+			HashMap<String, String> tmap = ts3conn.getConnector().getInfo(JTS3ServerQuery.INFOMODE_CLIENTINFO,
+					target.get()); // target
+			// infos
+			if (tmap.get(CLIENT_DB_KEY) == "") {
+				throw new NumberFormatException("missing " + CLIENT_DB_KEY);
 			}
+			dbID.set(Integer.valueOf(tmap.get(CLIENT_DB_KEY)));
 			List<Integer> channels = new ArrayList<Integer>();
 			Vector<HashMap<String, String>> rawChannelList = ts3conn.getConnector()
 					.getList(JTS3ServerQuery.LISTMODE_CHANNELLIST);
@@ -85,19 +90,36 @@ public class ModTools implements Mod {
 				}
 			}
 			logger.debug("Channels used for rocket:\n{}", channels);
-			for (int x = channels.size() - 1; x >= 0; x--) {
-				ts3conn.getConnector().moveClient(tid, channels.get(x), "");
-				ts3conn.getConnector().pokeClient(tid, "move");
+			int failed = 0;
+			for (int x = channels.size() - 1; x >= 0;) {
+				int sleep = 40;
 				try {
-					Thread.sleep(40);
+					if (target.get() != -1) {
+						ts3conn.getConnector().moveClient(target.get(), channels.get(x), "");
+						ts3conn.getConnector().pokeClient(target.get(), "move");
+						x--;
+					} else {
+						sleep = 500;
+					}
+				} catch (Exception e) {
+					logger.warn("rocket move/poke error {} {}", e.getMessage(), target.get());
+					sleep = 400;
+					failed++;
+					if (failed > (channels.size() / 2)) {
+						logger.debug("rocket break, too many retries");
+						break;
+					}
+				}
+				try {
+					Thread.sleep(sleep);
 				} catch (InterruptedException e) {
 					logger.warn(e);
 				}
 			}
-			ts3conn.getConnector().kickClient(tid, false, "You were rocketed!");
-			if (tid != applicant)
+			ts3conn.getConnector().kickClient(target.get(), false, "You were rocketed!");
+			if (!vetoActive)
 				ts3conn.getConnector().sendTextMessage(applicant, JTS3ServerQuery.TEXTMESSAGE_TARGET_CLIENT,
-						"[b]Client " + tmap.get("client_nickname") + "[" + tmap.get("client_database_id")
+						"[b]Client " + tmap.get("client_nickname") + "[" + tmap.get(CLIENT_DB_KEY)
 								+ "] was rocketed![/b]");
 		} catch (NumberFormatException e) {
 			try {
@@ -106,19 +128,49 @@ public class ModTools implements Mod {
 			} catch (TS3ServerQueryException e1) {
 				logger.fatal(e1);
 			}
-		} catch (TS3ServerQueryException e) {
+		} catch (Exception e) {
 			logger.error("Error on rocket \n{}", e);
 		} finally {
 			if (ts3conn != null)
 				ts3conn.disconnect();
-			rocketRunning.set(false);
+			running.set(false);
+			vetoActive = false;
+		}
+	}
+
+	/**
+	 * Thread running the rocket bot locally
+	 * 
+	 * @author aron
+	 *
+	 */
+	private class InternRocket extends Thread {
+		int sID;
+		boolean ignore_users;
+		AtomicInteger clientID;
+		AtomicBoolean runningBoolean;
+		AtomicInteger clientDBID;
+
+		public InternRocket(int sID, boolean ignore_users, AtomicInteger clientID, AtomicBoolean runningBoolean,
+				AtomicInteger clientDBID) {
+			super();
+			this.sID = sID;
+			this.ignore_users = ignore_users;
+			this.clientID = clientID;
+			this.runningBoolean = runningBoolean;
+			this.clientDBID = clientDBID;
+		}
+
+		@Override
+		public void run() {
+			rocketchan(clientID, clientDBID, runningBoolean, sID, ignore_users);
 		}
 	}
 
 	@Override
 	public void handleTextMessage(String eventType, HashMap<String, String> eventInfo) {
 		String[] args = eventInfo.get("msg").split(" ");
-		int sID = Integer.parseInt(eventInfo.get("invokerid"));
+		final int sID = Integer.parseInt(eventInfo.get("invokerid"));
 		try {
 			if (args[0].equals("!tools")) {
 				boolean permMissing = false;
@@ -128,10 +180,10 @@ public class ModTools implements Mod {
 				case "rocket":
 					if (isAdmin) {
 						if (args.length >= 3) {
-							boolean ignore_users = false;
-							if (args.length == 4)
-								ignore_users = args[3].equals("ignore");
-							rocketchan(args[2], sID, ignore_users);
+							final boolean ignore_users = args.length == 4 ? args[3].equals("ignore") : false;
+							clientID.getAndSet(Integer.valueOf(args[2]));
+							new InternRocket(sID, ignore_users, clientID, rocketRunning, dbID).start();
+							logger.debug("gone");
 						} else {
 							misCMD = true;
 						}
@@ -172,10 +224,33 @@ public class ModTools implements Mod {
 
 	@Override
 	public void handleClientJoined(HashMap<String, String> eventInfo) {
+		// if(rocketRunning.get() && Integer.valueOf(eventInfo.get(CLIENT_DB_KEY)) ==
+		// dbID.get()) {
+//		logger.debug("{} {}", eventInfo.get(CLIENT_ID_KEY));
+		if (rocketRunning.get()) {
+			if (Integer.valueOf(eventInfo.get(CLIENT_DB_KEY)) == dbID.get()) {
+				try {
+					instance.getTS3Connection().getConnector().kickClient(clientID.get(), false, "Fool.");
+				} catch (TS3ServerQueryException e) {
+					logger.error("{}", e);
+				}
+
+				clientID.set(Integer.valueOf(eventInfo.get(CLIENT_ID_KEY)));
+				logger.info("detected anti rocket move {}",clientID.get());
+			}
+		} else {
+//			logger.debug("rocket not running");
+		}
 	}
 
 	@Override
 	public void handleClientLeft(HashMap<String, String> eventInfo) {
+//		logger.debug(eventInfo.get(CLIENT_ID_KEY));
+		if (rocketRunning.get() && !eventInfo.get("reasonid").equals("5")
+				&& Integer.valueOf(eventInfo.get(CLIENT_ID_KEY)) == clientID.get()) { // 8 = kick, 5 = own leave
+			clientID.set(-1); // set invalid -> pause rocket
+			logger.debug("detected early leave");
+		}
 	}
 
 	@Override
@@ -184,7 +259,8 @@ public class ModTools implements Mod {
 
 	@Override
 	public ModRegisters registerEvents() {
-		return new ModRegisters.Builder().eventTextChannel(true).eventTextPrivate(true).eventTextServer(true).build();
+		return new ModRegisters.Builder().eventTextChannel(true).eventTextPrivate(true).eventTextServer(true)
+				.eventServer(true).build();
 	}
 
 	@Override
